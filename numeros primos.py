ea192356cc502654e8f2f56428457e046b7b64bf
Elton Marcolino <118011758+EltonMarcolino@users.noreply.github.com>
2023-01-24 16:00:09 -0300

    números primos

---
+++ numeros primos.py
@@ -0,0 +1,16 @@
+#  Programa que leia um número inteiro e diga se ele é ou não um número primo.
+
+numero = int(input('Digite um número: '))
+total = 0
+for c in range(1, numero + 1):
+    if numero % c == 0:
+        print('\033[33m', end=' ')
+        total += 1
+    else:
+        print('\033[31m', end=' ')
+    print('{}'.format(c), end=' ')
+print('\n\033[mO número {} foi divisível {} vezes.'.format(numero, total))
+if total == 2:
+    print('Ele é número PRIMO!')
+else:
+    print('Ele não é número PRIMO!')
